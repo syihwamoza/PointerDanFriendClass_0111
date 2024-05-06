6080d4c0db8be01b00d51136eaa86ee4233fa996
syihwamoza <mozakastella26@gmail.com>
2024-05-06 10:36:42 +0700

    add class mahasisswa

--- PointerDanFriendClass_0111/PointerDanFriendClass_0111.cpp
+++ PointerDanFriendClass_0111/PointerDanFriendClass_0111.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+using namespace std;
+class mahasiswa {
+      public:
+	  int nim;
+	  void showNim() {
+		  cout << "No Induk = " << nim << endl;
+	  }
+};
 
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+
 
